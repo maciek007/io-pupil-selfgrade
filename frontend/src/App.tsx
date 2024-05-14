0f8425fdc3809717703c2f4d0d92531dfa3a7efe
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-05-14 21:12:34 +0200

    Define minimum interaction for backend and frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from '/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const fetchData = () => {
+    axios.get("http://127.0.0.1:9000/helloWorld").then(res => {
+      console.log(res);
+      setData(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>{data}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
