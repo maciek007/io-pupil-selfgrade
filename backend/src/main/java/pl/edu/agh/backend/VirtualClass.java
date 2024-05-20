90cea495b1a74e16cdf9e2cced659d24d3ca1aa4
Dominik Puz <dominikus.puz@gmail.com>
2024-05-20 18:07:50 +0200

    created virtual class object

---
+++ backend/src/main/java/pl/edu/agh/backend/VirtualClass.java
@@ -0,0 +1,36 @@
+package pl.edu.agh.backend;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Random;
+
+@Getter
+public class VirtualClass {
+    private final String className;
+    private final HashMap<String, Student> students;
+    private final String accessCode;
+
+    public VirtualClass(String className) {
+        this.className = className;
+        students = new HashMap<>();
+        accessCode = generateAccessCode();
+    }
+
+    private String generateAccessCode() {
+        Random random = new Random();
+        return String.valueOf(random.nextInt(10000, 100000));
+    }
+
+    public boolean addStudent(Student student) {
+        if (students.containsKey(student.getName())) {
+            return false;
+        }
+        students.put(student.getName(), student);
+        return true;
+    }
+
+    public boolean removeStudent(String name) {
+        return students.remove(name) != null;
+    }
+}
