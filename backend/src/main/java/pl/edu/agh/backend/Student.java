ab8a2264f88bea71256bc55c9e85e87a189df0e5
Dominik Puz <dominikus.puz@gmail.com>
2024-05-23 10:42:17 +0200

    implemented student class

--- backend/src/main/java/pl/edu/agh/backend/Student.java
+++ backend/src/main/java/pl/edu/agh/backend/Student.java
@@ -1,12 +1,33 @@
 package pl.edu.agh.backend;
 
 import lombok.Getter;
+import pl.edu.agh.backend.models.Form;
+
+import java.util.HashMap;
 
 @Getter
 public class Student {
-    private String name;
+    private final String name;
+    private final HashMap<String, Form> forms;
 
     public Student(String name) {
         this.name = name;
+        forms = new HashMap<>();
+    }
+
+    public boolean addForm(String name, Form form) {
+        if (!forms.containsKey(name)) {
+            forms.put(name, form);
+            return true;
+        }
+        return false;
+    }
+
+    public void removeForm(String name) {
+        forms.remove(name);
+    }
+
+    public void addForms(HashMap<String, Form> forms) {
+        this.forms.putAll(forms);
     }
 }
