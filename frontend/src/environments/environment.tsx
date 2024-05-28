87de1f974ebb5dc3d4a89e95091a926bb4c5b72a
Dominik Puz <dominikus.puz@gmail.com>
2024-05-28 10:47:03 +0200

    preparation for presentation

--- frontend/src/environments/environment.tsx
+++ frontend/src/environments/environment.tsx
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    // backEnd: "http://localhost:8080/api/v1",
-    backEnd: "http://192.168.43.76:8080/api/v1",
+    backEnd: "http://localhost:8080/api/v1",
+    // backEnd: "http://192.168.43.76:8080/api/v1",
 };
